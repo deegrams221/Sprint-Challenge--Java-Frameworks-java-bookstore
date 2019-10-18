5e8b39c3ca67ac172a232e7132e66fe47797280c
Diana Grams <deegrams221@gmail.com>
2019-10-18 10:07:33 -0600

    added AuthorController and BookController

--- StartHere/src/main/java/com/lambdaschool/starthere/controllers/AuthorController.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/controllers/AuthorController.java
@@ -1,7 +1,54 @@
 package com.lambdaschool.starthere.controllers;
 
-public class AuthorController {
+import com.lambdaschool.starthere.models.Authors;
+import com.lambdaschool.starthere.services.AuthorService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
-    // GET /authors/authors - returns a JSON object list of all the authors, their books, and the book's section.
+@RestController
+@RequestMapping(value = "/authors")
+public class AuthorController
+{
+    @Autowired
+    private AuthorService authorService;
+
+    // Adding custom swagger documentation for list all Authors with paging/sorting
+    @ApiOperation(value = "Return all Authors",
+            response = Authors.class,
+            responseContainer = "List")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page",
+                    dataType = "integer",
+                    paramType = "query",
+                    value = "Results page you want to retrieve (0..N)"),
+            @ApiImplicitParam(name = "size",
+                dataType = "integer",
+                paramType = "query",
+                value = "Number of records per page."),
+            @ApiImplicitParam(name = "sort",
+                allowMultiple = true,
+                dataType = "string",
+                paramType = "query",
+                value = "Sorting criteria in the format: property(,asc|desc). "
+                        + "Default sort order is ascending. "
+                        + "Multiple sort criteria are supported.")})
+
+    // GET /authors/authors - returns a JSON object list of all the authors,
+    // their books, and the book's section.
+    @GetMapping(value = "/authors")
+    public ResponseEntity<?> findAllAuthors(Pageable pageable)
+    {
+        List<Authors> authorList = authorService.findAll(pageable);
+        return new ResponseEntity<>(authorList, HttpStatus.OK);
+    }
 }
--- StartHere/src/main/java/com/lambdaschool/starthere/controllers/BookController.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/controllers/BookController.java
@@ -1,22 +1,128 @@
 package com.lambdaschool.starthere.controllers;
 
 
+import com.lambdaschool.starthere.models.Authors;
+import com.lambdaschool.starthere.models.Book;
+import com.lambdaschool.starthere.models.ErrorDetail;
+import com.lambdaschool.starthere.services.BookService;
+import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+public class BookController
+{
+    @Autowired
+    private BookService bookService;
 
-public class BookController {
-
+    // Adding custom swagger documentation for list all Authors with paging/sorting
+    @ApiOperation(value = "Return all Books",
+            response = Book.class,
+            responseContainer = "List")
+    @ApiImplicitParams({@ApiImplicitParam(name = "page",
+                dataType = "integer",
+                paramType = "query",
+                value = "Results page you want to retrieve (0..N)"),
+            @ApiImplicitParam(name = "size",
+                dataType = "integer",
+                paramType = "query",
+                value = "Number of records per page."),
+            @ApiImplicitParam(name = "sort",
+                allowMultiple = true,
+                dataType = "string",
+                paramType = "query",
+                value = "Sorting criteria in the format: property(,asc|desc). "
+                        + "Default sort order is ascending. "
+                        + "Multiple sort criteria are supported.")})
 
     // GET /books/books - returns a JSON object list of all the books, thier sections, and their authors.
+    @GetMapping(value = "/books/books")
+    public ResponseEntity<?> findAllBooks(Pageable pageable)
+    {
+        return new ResponseEntity<>(bookService.findAll(pageable), HttpStatus.OK);
+    }
 
+
+    // Adding custom swagger documentation for update book
+    @ApiOperation(value = "Update a book",
+            response = void.class)
+    @ApiResponses(value = {@ApiResponse(code = 200,
+            message = "Book Information Updated",
+            response = void.class),
+            @ApiResponse(code = 500,
+                    message = "Could Not Update Book",
+                    response = ErrorDetail.class)})
 
     // PUT /data/books/{id} - updates a books info (Title, Copyright, ISBN) but
     // does NOT have to assign authors to the books.
+    @PutMapping(value = "/data/books/{id}")
+    public ResponseEntity<?> updateBook(
+            // Adding custom swagger params for update book
+            @ApiParam(value = "Book Id",
+                    required = true,
+                    example = "1")
+            @PathVariable
+                    long id,
+            @RequestBody
+                    Book book)
+    {
+        bookService.updateBook(book, id);
+        return new ResponseEntity<>(book, HttpStatus.OK);
+    }
 
+    // Adding custom swagger documentation for assigning a book
+    @ApiOperation(value = "Assigns a book already in the system",
+            notes = "Book Id will be sent to the location header",
+            response = void.class)
+    @ApiResponses(value = {@ApiResponse(code = 201,
+            message = "Book assigned",
+            response = void.class),
+            @ApiResponse(code = 500,
+                    message = "Could Not Assign Book",
+                    response = ErrorDetail.class)})
 
     // POST /data/books/{bookid}/authors/{authorid} - assigns a book already in the system
     // (bookid) to an author already in the system (authorid) (see how roles are handled for users)
+    @PostMapping(value = "/data/books/{bookid}/authors/{authorid}")
+    public ResponseEntity<?> matchBookWithAuthor(
+            // Adding custom swagger params for assigning book
+            @ApiParam(value = "Book Id",
+                    required = true,
+                    example = "1")
+            @PathVariable
+                    long id,
+            @RequestBody
+                    Authors authors)
+    {
+        bookService.assignAuthor(id, authors.getAuthorid());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
+    // Adding custom swagger documentation for deleting a book
+    @ApiOperation(value = "Delete a book",
+            response = void.class)
+    @ApiResponses(value = {@ApiResponse(code = 201,
+            message = "Book Deleted",
+            response = void.class),
+            @ApiResponse(code = 500,
+                    message = "Could Not Delete Book",
+                    response = ErrorDetail.class)})
 
     // DELETE /data/books/{id} - deletes a book and the book author combinations -
     // but does not delete the author records.
+    @DeleteMapping(value = "/data/books/{id}")
+    public ResponseEntity<?> deleteBook(
+            // Adding custom swagger params for delete book
+            @ApiParam(value = "Book Id",
+                    required = true,
+                    example = "1")
+            @PathVariable
+                    long id)
+    {
+        bookService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
